d9365510b9faffd9e12fb4ca54500ae1c622748c
shahed-bd71 <mdshahedbs71@gmail.com>
2023-01-17 19:33:58 +0600

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { apiSlice } from "../Api/apiSlice";
+import { apiSlice } from "./Api/apiSlice";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
