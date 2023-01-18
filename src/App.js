2d7fd68fcaef33e1b57feafd5b9e071fcf14e2b1
shahed-bd71 <mdshahedbs71@gmail.com>
2023-01-18 19:01:33 +0600

    fix title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostList from './components/Job';
 function App() {
   return (
     <div className="container mx-auto my-5">
+      <h3 className='h3 text-center my-5'>Jobs Board</h3>
       <PostList/>
     </div>
   );
